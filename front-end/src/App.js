a08f34ceae7b848464c00d9325cc0c0d288529b4
Thiago Sousa <thyagosousasilva@gmail.com>
2020-03-26 18:39:34 -0300

    Aprendendo sobre estados
    * Implementando simples exemplo de contador incrementado por evento acionado apartir de um botão. NEsta atualização, aprendi a lidar com estados no React.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
 
 function App() {
+  let [counter, setCounter] = useState(0);
+
+  function increment() {
+    setCounter(counter + 1);
+    console.log(counter);
+  }
+
   return (
-    <Header title='Thiago Silva'>Be a Hero</Header>
+    <div>
+      <Header>Contador: {counter}</Header>
+      <button onClick={increment}>Adicionar</button>
+    </div>
+
   );
 }
 
